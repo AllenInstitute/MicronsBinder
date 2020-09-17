ff8f84770714ce7af06f013a5972629c4431e0be
amwilson149 <amw7@princeton.edu>
2020-09-16 22:46:05 -0400

    Updating seg ID tables to download.

--- notebooks/multiscale/resource_statistics/downloadData.sh
+++ notebooks/multiscale/resource_statistics/downloadData.sh
@@ -3,8 +3,8 @@ mkdir -p data/soma_ids
 mkdir -p data/smoothed_skels
 mkdir -p data/resource_vis_info
 
-gdown --id 1_z2OxTGZ8VQNiiY-Ktj7L5M1pzwNFZp2 -O data/soma_ids/p100_pyr_soma_IDs_v185.csv
-gdown --id 1_r7cnq-ZoeSqR_FGhVhLMPpL5h8Skb_9 -O data/soma_ids/p100_inh_soma_IDs_v185.csv
+gdown --id 1_Xciy8oPcpegRMuM3AUNU7tpHgHtrLlv -O data/soma_ids/p100_pyr_soma_IDs_v185.csv
+gdown --id 1k3XtA3LPcK3MN3BwUIRKcqJEU9PTi3Gz -O data/soma_ids/p100_inh_soma_IDs_v185.csv
 
 gdown --id 1Tpa-p7rwOZ65rIwDPbm4famUFtqhbqOK -O data/smoothed_skels/smoothed_skeletons_v185.tgz \
 && pushd data/smoothed_skels \
