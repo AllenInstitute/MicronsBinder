24da7c5cf5d92c37b18932570bfd2d83b43ed45c
nicholasturner1 <nturner.stanford@gmail.com>
2021-04-22 15:38:09 -0400

    Updating derivedata.sh

--- notebooks/multiscale/mitochondria/scripts/derivedata.sh
+++ notebooks/multiscale/mitochondria/scripts/derivedata.sh
@@ -4,7 +4,7 @@
 # NOTE: this requires downloading the meshes
 #   (through downloadNeuronMeshes.sh)
 python scripts/computebasicbranchstats.py \
-    data/pycids.csv \
+    data/clean_and_complete_soma_ids_v185.csv \
     data/basicbranchstats.csv
 
 
@@ -16,8 +16,7 @@ python scripts/mitotoskel.py \
     data/temp/assoc_
 python scripts/assemblemitotoskel.py \
     data/pni_mito_cellswskel_v185.csv \
-    data/pycids.csv data/mitotoskel.h5
-
+    data/pycids_v185.csv data/mitotoskel.h5
 
 # Computing MCI and assigning mitochondria to compartments
 # NOTE: this requires downloading the mitochondria meshes
@@ -25,16 +24,16 @@ python scripts/assemblemitotoskel.py \
 python scripts/computeextramitostats.py \
     data/pni_mito_cellswskel_v185.csv \
     data/mitotoskel.h5 \
-    data/fullmitostats.csv
+    data/pni_mito_cellswskel_v185_fullstats.csv
 
 
 # Computing density statistics
 python scripts/computedensitystats.py \
     data/pni_mito_cellswskel_v185_fullstats.csv \
-    data/pycids.csv data/pycinputs.csv \
+    data/pycids_v185.csv data/neuron_received_synapses_v185.csv \
     data/basicbranchstats.csv \
-    data/mitochondriaoverlap.csv \
+    data/pni_mito_cell_overlaps_v185.csv \
     data/pni_nucleus_segments.csv \
-    data/pni_nucleus_lookup.csv \
+    data/clean_and_complete_nucleus_lookup.csv \
     data/fullbranchstats.csv \
     data/cellwisestats.csv
